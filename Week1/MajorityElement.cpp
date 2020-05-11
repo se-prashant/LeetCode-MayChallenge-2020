5b3b42eb7e55f629b18cf73e0e26abb808222f88
PRASHANT SINGH <prashant791singh@gmail.com>
2020-05-11 20:29:03 +0530

    Week1_May6
    https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3321/

---
+++ Week1/MajorityElement.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3321/
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
